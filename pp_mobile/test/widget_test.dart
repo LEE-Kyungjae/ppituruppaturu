c7fff1c78950dd36ef8f43694f8bd39b0c2e73ed
LEE-Kyungjae <ze2@kakao.com>
2025-09-23 23:55:15 +0900

    test(mobile): add fake webview platform for widget test

--- pp_mobile/test/widget_test.dart
+++ pp_mobile/test/widget_test.dart
@@ -2,22 +2,247 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
 import 'package:pp_mobile/main.dart';
 
+class _FakeWebViewPlatform extends WebViewPlatform {
+  @override
+  PlatformWebViewCookieManager createPlatformCookieManager(
+    PlatformWebViewCookieManagerCreationParams params,
+  ) {
+    return _FakeCookieManager(params);
+  }
+
+  @override
+  PlatformNavigationDelegate createPlatformNavigationDelegate(
+    PlatformNavigationDelegateCreationParams params,
+  ) {
+    return _FakeNavigationDelegate(params);
+  }
+
+  @override
+  PlatformWebViewController createPlatformWebViewController(
+    PlatformWebViewControllerCreationParams params,
+  ) {
+    return _FakeWebViewController(params);
+  }
+
+  @override
+  PlatformWebViewWidget createPlatformWebViewWidget(
+    PlatformWebViewWidgetCreationParams params,
+  ) {
+    return _FakeWebViewWidget(params);
+  }
+}
+
+class _FakeCookieManager extends PlatformWebViewCookieManager {
+  _FakeCookieManager(PlatformWebViewCookieManagerCreationParams params)
+    : super.implementation(params);
+
+  @override
+  Future<bool> clearCookies() async => true;
+
+  @override
+  Future<void> setCookie(WebViewCookie cookie) async {}
+}
+
+class _FakeNavigationDelegate extends PlatformNavigationDelegate {
+  _FakeNavigationDelegate(PlatformNavigationDelegateCreationParams params)
+    : super.implementation(params);
+
+  @override
+  Future<void> setOnNavigationRequest(
+    NavigationRequestCallback onNavigationRequest,
+  ) async {}
+
+  @override
+  Future<void> setOnPageStarted(PageEventCallback onPageStarted) async {}
+
+  @override
+  Future<void> setOnPageFinished(PageEventCallback onPageFinished) async {}
+
+  @override
+  Future<void> setOnHttpError(HttpResponseErrorCallback onHttpError) async {}
+
+  @override
+  Future<void> setOnProgress(ProgressCallback onProgress) async {}
+
+  @override
+  Future<void> setOnWebResourceError(
+    WebResourceErrorCallback onWebResourceError,
+  ) async {}
+
+  @override
+  Future<void> setOnUrlChange(UrlChangeCallback onUrlChange) async {}
+
+  @override
+  Future<void> setOnHttpAuthRequest(
+    HttpAuthRequestCallback onHttpAuthRequest,
+  ) async {}
+
+  @override
+  Future<void> setOnSSlAuthError(SslAuthErrorCallback onSslAuthError) async {}
+}
+
+class _FakeWebViewController extends PlatformWebViewController {
+  _FakeWebViewController(PlatformWebViewControllerCreationParams params)
+    : super.implementation(params);
+
+  @override
+  Future<void> addJavaScriptChannel(
+    JavaScriptChannelParams javaScriptChannelParams,
+  ) async {}
+
+  @override
+  Future<bool> canGoBack() async => false;
+
+  @override
+  Future<bool> canGoForward() async => false;
+
+  @override
+  Future<void> clearCache() async {}
+
+  @override
+  Future<void> clearLocalStorage() async {}
+
+  @override
+  Future<String?> currentUrl() async => null;
+
+  @override
+  Future<void> enableZoom(bool enabled) async {}
+
+  @override
+  Future<void> goBack() async {}
+
+  @override
+  Future<void> goForward() async {}
+
+  @override
+  Future<Offset> getScrollPosition() async => Offset.zero;
+
+  @override
+  Future<String?> getTitle() async => null;
+
+  @override
+  Future<String?> getUserAgent() async => null;
+
+  @override
+  Future<void> loadFile(String absoluteFilePath) async {}
+
+  @override
+  Future<void> loadFlutterAsset(String key) async {}
+
+  @override
+  Future<void> loadHtmlString(String html, {String? baseUrl}) async {}
+
+  @override
+  Future<void> loadRequest(LoadRequestParams params) async {}
+
+  @override
+  Future<void> reload() async {}
+
+  @override
+  Future<void> removeJavaScriptChannel(String javaScriptChannelName) async {}
+
+  @override
+  Future<void> runJavaScript(String javaScript) async {}
+
+  @override
+  Future<Object> runJavaScriptReturningResult(String javaScript) async => {};
+
+  @override
+  Future<void> scrollBy(int x, int y) async {}
+
+  @override
+  Future<void> scrollTo(int x, int y) async {}
+
+  @override
+  Future<void> setBackgroundColor(Color color) async {}
+
+  @override
+  Future<void> setHorizontalScrollBarEnabled(bool enabled) async {}
+
+  @override
+  Future<void> setJavaScriptMode(JavaScriptMode javaScriptMode) async {}
+
+  @override
+  Future<void> setOnConsoleMessage(
+    void Function(JavaScriptConsoleMessage consoleMessage) onConsoleMessage,
+  ) async {}
+
+  @override
+  Future<void> setOnJavaScriptAlertDialog(
+    Future<void> Function(JavaScriptAlertDialogRequest request)
+    onJavaScriptAlertDialog,
+  ) async {}
+
+  @override
+  Future<void> setOnJavaScriptConfirmDialog(
+    Future<bool> Function(JavaScriptConfirmDialogRequest request)
+    onJavaScriptConfirmDialog,
+  ) async {}
+
+  @override
+  Future<void> setOnJavaScriptTextInputDialog(
+    Future<String> Function(JavaScriptTextInputDialogRequest request)
+    onJavaScriptTextInputDialog,
+  ) async {}
+
+  @override
+  Future<void> setOnPlatformPermissionRequest(
+    void Function(PlatformWebViewPermissionRequest request) onPermissionRequest,
+  ) async {}
+
+  @override
+  Future<void> setOnScrollPositionChange(
+    void Function(ScrollPositionChange scrollPositionChange)? onScrollChanged,
+  ) async {}
+
+  @override
+  Future<void> setOverScrollMode(WebViewOverScrollMode mode) async {}
+
+  @override
+  Future<void> setPlatformNavigationDelegate(
+    PlatformNavigationDelegate handler,
+  ) async {}
+
+  @override
+  Future<void> setUserAgent(String? userAgent) async {}
+
+  @override
+  Future<void> setVerticalScrollBarEnabled(bool enabled) async {}
+}
+
+class _FakeWebViewWidget extends PlatformWebViewWidget {
+  _FakeWebViewWidget(PlatformWebViewWidgetCreationParams params)
+    : super.implementation(params);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
+
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  WebViewPlatform.instance = _FakeWebViewPlatform();
+
   testWidgets('PittuRu app loads correctly', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const PittuRuApp());
 
-    // Verify that app title is displayed
-    expect(find.text('🎮 PittuRu PpattuRu'), findsOneWidget);
-    
-    // Verify that navigation buttons are present
-    expect(find.byIcon(Icons.arrow_back), findsOneWidget);
-    expect(find.byIcon(Icons.arrow_forward), findsOneWidget);
-    expect(find.byIcon(Icons.refresh), findsOneWidget);
-    expect(find.byIcon(Icons.language), findsOneWidget);
-    expect(find.byIcon(Icons.menu), findsOneWidget);
+    await tester.pumpAndSettle();
+
+    // Verify that the main hub title is displayed
+    expect(find.text('PittuRu Game Hub'), findsOneWidget);
+
+    // Verify that key UI sections render
+    expect(find.text('게임을 선택하세요'), findsOneWidget);
+    expect(find.text('랭킹'), findsOneWidget);
+
+    // Verify that tab icons are present
+    expect(find.byIcon(Icons.games), findsWidgets);
+    expect(find.byIcon(Icons.leaderboard), findsWidgets);
   });
 }
